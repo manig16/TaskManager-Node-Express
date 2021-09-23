ec70ed4d254bbe7db9aec74128489cf2836a76b0
Mani <Ramachandran>
2021-09-23 04:09:11 -0400

    add Mongo db connectivity

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const tasks = require('./routes/taskRoute')
+const connectMongoDB = require('./database/connect')
+require('dotenv').config()
 
 const port = 3000
 
@@ -18,6 +20,13 @@ app.use('/api/v1/tasks', tasks)
 // update a task
 // delete a task
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-})
+const start = async() => {
+    try {
+        await connectMongoDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server listening on port ${port}`))
+    } catch (error) {
+        console.log(err)
+    }
+}
+
+start()
